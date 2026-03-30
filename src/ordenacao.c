b62047aa126970f70b5488a84923f4fd731cdb63
henriqueetiene <henriqueetienenanidesouza@gmail.com>
2026-03-30 13:16:26 -0400

    implementação das funções de ordenação

---
+++ src/ordenacao.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include "ordenacao.h"
+
+void bubleSort(int *v, int n)
+{
+    int continua, aux, fim = n;
+    do
+    {
+        continua = 0;
+        for (int i = 0; i < fim - 1; i++)
+        {
+            if (v[i] > v[i + 1])
+            {
+                aux = v[i];
+                v[i] = v[i + 1];
+                v[i + 1] = aux;
+                continua = i + 1;
+            }
+        }
+    } while (continua != 0);
+}
+
+void insertionSort(int *v, int n)
+{
+    int aux, j;
+    for (int i = 1; i < n; i++)
+    {
+        aux = v[i];
+        for (j = i; (j > 0) && (aux < v[j - 1]); j--)
+        {
+            v[j] = v[j - 1];
+        }
+        v[j] = aux;
+    }
+}
+
+void selectionSort(int *v, int n)
+{
+    int minIndex, aux;
+    for (int i = 0; i < n; i++)
+    {
+        minIndex = i;
+        for (int j = i + 1; j < n; i++)
+        {
+            if (v[j] < v[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+        if (i != minIndex)
+        {
+            aux = v[minIndex];
+            v[minIndex] = v[i];
+            v[i] = aux;
+        }
+    }
+}
