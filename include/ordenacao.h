bcec42a96b720e0ce3c88eb8eb628c3383a6fa65
henriqueetiene <henriqueetienenanidesouza@gmail.com>
2026-04-12 14:28:14 -0400

    implementa algoritmo quick sort

--- include/ordenacao.h
+++ include/ordenacao.h
@@ -5,3 +5,7 @@ void bubleSort(int *v, int n);
 void selectionSort(int *v, int n);
 
 void insertionSort(int *v, int n);
+
+int particiona(int *v, int inicio, int final);
+
+void quickSort(int *v, int inicio, int fim);
--- src/ordenacao.c
+++ src/ordenacao.c
@@ -56,3 +56,46 @@ void selectionSort(int *v, int n)
         }
     }
 }
+
+int particiona(int *v, int inicio, int final)
+{
+    int esq, dir, pivo, aux;
+    
+    esq = inicio;
+    dir = final;
+    pivo = v[inicio];
+
+    while (esq < dir)
+    {
+        while (esq <= final && v[esq] <= pivo)
+        {
+            esq++;
+        }
+        while (dir >= 0 && v[dir] > pivo)
+        {
+            dir--;
+        }
+        if (esq < dir)
+        {
+            aux = v[esq];
+            v[esq] = v[dir];
+            v[dir] = aux;
+        }
+    }
+    v[inicio] = v[dir];
+    v[dir] = pivo;
+
+    return dir;
+}
+
+void quickSort(int *v, int inicio, int fim)
+{
+    int pivo;
+
+    if (fim > inicio)
+    {
+        pivo = particiona(v, inicio, fim);
+        quickSort(v, inicio, pivo - 1);
+        quickSort(v, pivo + 1, fim);
+    }
+}
